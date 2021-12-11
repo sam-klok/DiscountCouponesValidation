974d8e8faec1e1da47056e0e2cc6a41bfa99a34b
sam-klok <sklok11@gmail.com>
2021-12-11 14:53:42 -0700

    Initial commit

---
+++ DiscountCouponesValidation/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscountCouponesValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var discounts = new List<string> { "a", "abba", "abca" };
+            Print(findValidDiscountCoupons(discounts));
+
+            Console.WriteLine("press any key..");
+            Console.ReadKey();
+        }
+
+        public static List<int> findValidDiscountCoupons(List<string> discounts)
+        {
+            var r = new List<int>(); // result
+            foreach (var d in discounts)
+            {
+                if (d == "")
+                    r.Add(1);
+                else if (d.Length == 1)
+                    r.Add(1);
+                else
+                {
+                    if (isAllCharCountEven(d))
+                        r.Add(1);
+                    else
+                        r.Add(0);
+                }
+            }
+
+            return r;
+        }
+
+        public static bool isAllCharCountEven(string s)
+        {
+            while (s.Length > 0)
+            {
+                int count = 0;
+                for (int j = 0; j < s.Length; j++)
+                {
+                    if (s[0] == s[j])
+                    {
+                        count++;
+                    }
+                }
+
+                if (count % 2 != 0)
+                    return false;
+
+                s = s.Replace(s[0].ToString(), string.Empty);
+            }
+
+            return true;
+        }
+
+        public static void Print(List<int> r)
+        {
+            if (r == null)
+                Console.Write("null");
+            else
+                foreach (var i in r)
+                    Console.Write(i + " ");
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+Please look to attached images for the task.
+This solution is raw, and not final at all.
+
+Thank you
+Sam Klok
